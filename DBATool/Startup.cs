1c01804aa8c2a098b0537a9f5c8d684822280204
Roy L Moore <roy.l.moore@hotmail.com>
2019-01-19 13:29:56 -0500

    add connection

--- DBATool/Startup.cs
+++ DBATool/Startup.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DBATool.Data;
+using DBATool.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,6 +34,13 @@ namespace DBATool
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddScoped<IDatabase, DatabaseService>();
+            services.AddScoped<IEmployee, EmployeeService>();
+            services.AddScoped<IServer, ServerService>();
+            services.AddScoped<IStatus, StatusService>();
+
+            services.AddDbContext<DBAToolDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DBAToolsConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
