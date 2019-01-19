06395ff636a1681fa10478df3dc012476ace03e7
Roy L Moore <roy.l.moore@hotmail.com>
2019-01-19 10:43:39 -0500

    Add project files.

---
+++ DBATool/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DBATool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
